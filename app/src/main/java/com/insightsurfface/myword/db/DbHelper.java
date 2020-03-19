5a2a2f9556d1cf103e66bbb889477cb9dd47f1f3
warriorWorld <772192594@qq.com>
2020-03-19 17:37:54 +0800

    Initial commit

---
+++ app/src/main/java/com/insightsurfface/myword/db/DbHelper.java
@@ -0,0 +1,32 @@
+package com.insightsurfface.myword.db;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteDatabase.CursorFactory;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class DbHelper extends SQLiteOpenHelper {
+    private Context xcontext;
+    // 数据库名
+    private static final String DATABASE_NAME = "myword.db";
+    // 表名
+    public static final String WORDS_TABLE_NAME = "WordsBook";
+    private static final int DATABASE_VERSION = 1;
+    public static final String WORDS_BOOK = "create table if not exists " + WORDS_TABLE_NAME + " ("
+            + "id integer primary key autoincrement,"
+            + "word text," + "time integer," + "kill_time integer," + "update_time long," + "example_path text," + "createdtime TimeStamp NOT NULL DEFAULT (datetime('now','localtime')))";
+
+    public DbHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+        xcontext = context;
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL(WORDS_BOOK);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+    }
+}
