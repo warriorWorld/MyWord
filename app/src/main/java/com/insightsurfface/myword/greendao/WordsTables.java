3baa47ed57da81a98ae4e0d91a9e48ba9724d1df
warriorWorld <772192594@qq.com>
2020-04-23 16:53:43 +0800

    surpass zhimi

--- app/src/main/java/com/insightsurfface/myword/greendao/WordsTables.java
+++ app/src/main/java/com/insightsurfface/myword/greendao/WordsTables.java
@@ -10,30 +10,38 @@ public class WordsTables {
     private long id;
     private String name;
     private String url;
+
     @Generated(hash = 1433885187)
     public WordsTables(long id, String name, String url) {
         this.id = id;
         this.name = name;
         this.url = url;
     }
+
     @Generated(hash = 514604033)
     public WordsTables() {
     }
+
     public long getId() {
         return this.id;
     }
+
     public void setId(long id) {
         this.id = id;
     }
+
     public String getName() {
         return this.name;
     }
+
     public void setName(String name) {
         this.name = name;
     }
+
     public String getUrl() {
         return this.url;
     }
+
     public void setUrl(String url) {
         this.url = url;
     }
